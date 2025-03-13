d62ba37a638a78d71392a3cd7186971eef13a62b
pusingdengandunia <amedikenthuamediketu@gmail.com>
2025-03-13 20:08:35 +0700

    Update dsotm.sh

--- soal_3/dsotm.sh
+++ soal_3/dsotm.sh
@@ -70,9 +70,11 @@ while :
               c=$4
               letter=substr(letters,c,1)
               a[$3]=0
-              for (x in a) {o=a[x];a[x]=a[x]+1
-              printf "\033[%s;%sH\033[2;32m%s",o,x,letter
-              printf "\033[%s;%sH\033[1;37m%s\033[0;0H",a[x],x,letter
+              for (x in a) {
+              o=a[x];
+              a[x]=a[x]+1;
+              printf "\033[%s;%sH\033[2;32m%s",o,x,letter;
+              printf "\033[%s;%sH\033[1;37m%s\033[0;0H",a[x],x,letter;
               if (a[x] >= $1) { a[x]=0; } }}'
 #untuk brain damage
 elif [ "$TRACK" = "Brain Damage" ]
