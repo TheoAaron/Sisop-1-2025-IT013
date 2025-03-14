f53ebc3ea824c61766c2c674ff2e1d79dc3a8739
pusingdengandunia <amedikenthuamediketu@gmail.com>
2025-03-15 06:01:59 +0700

    Update frag_monitor.sh

--- soal_2/scripts/frag_monitor.sh
+++ soal_2/scripts/frag_monitor.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-watch -n 1 "free -m | awk '/Mem/ {printf \"RAM Usage: %.2f%%\\n\", \$3/\$2 * 100.0}'"
+watch -n 1 "free -m| awk '/Mem/ {printf \"RAM Usage : %.2f%%\", \$3/\$2 * 100.0}'"
