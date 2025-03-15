56adfcedc135964a2177b23f649d7b32c64bf30f
pusingdengandunia <amedikenthuamediketu@gmail.com>
2025-03-15 21:22:38 +0700

    Update frag_monitor.sh

--- soal_2/scripts/frag_monitor.sh
+++ soal_2/scripts/frag_monitor.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 
-watch -n 1 "free -m| awk '/Mem/ {printf \"RAM Usage : %.2f%%\", \$3/\$2 * 100.0}'"
+DETIK=1
+
+while true; do
+  RAM_USG=$(free | grep Mem | awk '{print $3/$2 * 100.0}')
+  RAM_TOTAL=$(free -m | grep Mem | awk '{print $2}')
+  RAM_AVAILABLE=$(free -m | grep Mem | awk '{print $7}')
+  RAM_FRAGMENT=$(vmstat -s | grep "pages paged out" | awk '{print $1}')
+
+  TIME=$(date +"%Y-%m-%d %H:%M:%S")
+
+  clear
+
+  echo "RAM"
+  echo "[$TIME] - Fragment Usage [$RAM_USG%] - Fragment Count [$RAM_FRAGMENT MB] - Details [Total: $RAM_TOTAL MB, Avaliable: $RAM_AVAILABLE MB]"
+
+  sleep $DETIK
+done
