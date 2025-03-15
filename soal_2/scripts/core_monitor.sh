8682a0127c17c93f83561d52200179c205f01e77
pusingdengandunia <amedikenthuamediketu@gmail.com>
2025-03-15 21:55:28 +0700

    Update core_monitor.sh

--- soal_2/scripts/core_monitor.sh
+++ soal_2/scripts/core_monitor.sh
@@ -1,16 +1,9 @@
 #!/bin/bash
 
-TIME=1
-
 while true; do
- CPU_USAGE=$(top -bn1 | grep "Cpu(s)" | sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | awk '{print 100 - $1}')
- CPU_MODEL=$(lscpu | grep "Model name" | cut -d ':' -f 2 | xargs)
-
- RECENT=$(date +"%Y-%m-%d %H:%M:%S")
- clear
-     
- echo "CPU"
- echo "[$RECENT] – Core Usage [$CPU_USAGE%] – Terminal Model [$CPU_MODEL]"
-
- sleep $TIME
+clear
+echo "CPU OVERALL :" 
+grep 'cpu ' /proc/stat | awk '{usage=($2+$4)*100/($2+$4+$5)} END {print usage "%"}'
+lscpu | grep "Model name" | sed "s/Model name:/CPU Model :/"
+sleep 1
 done
